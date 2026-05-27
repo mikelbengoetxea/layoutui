43b7daa9261104ea22a5c3701b931296b3029fbb
mikelbengoetxea <mikelbanl@gmail.com>
2026-05-27 12:45:25 +0200

    export test

--- dxf-export.js
+++ dxf-export.js
@@ -1,36 +1,98 @@
 /**
- * Minimal DXF R12-style export (mm). Layout coords: origin bottom-left, y up.
+ * DXF export (R12 / AC1009) in mm. Layout coords: origin bottom-left, y up.
+ * Uses classic POLYLINE + VERTEX + SEQEND for broad AutoCAD compatibility.
  */
 (function (global) {
   "use strict";
 
-  function num(n) {
-    return Number(n).toFixed(4);
+  function pair(code, value) {
+    return String(code).padStart(3, " ") + "\r\n" + String(value) + "\r\n";
   }
 
+  function num(n) {
+    return Number(n).toFixed(6);
+  }
+
+  function section(name) {
+    return pair(0, "SECTION") + pair(2, name);
+  }
+
+  function endSection() {
+    return pair(0, "ENDSEC");
+  }
+
+  function bounds(layout) {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+    for (const r of layout.rects) {
+      minX = Math.min(minX, r.x);
+      minY = Math.min(minY, r.y);
+      maxX = Math.max(maxX, r.x + r.w);
+      maxY = Math.max(maxY, r.y + r.h);
+    }
+    if (!layout.rects.length) {
+      minX = minY = 0;
+      maxX = layout.areaWidth;
+      maxY = layout.areaHeight;
+    }
+    return { minX, minY, maxX, maxY };
+  }
+
+  /** Closed rectangle as R12 POLYLINE */
   function rectPolyline(x, y, w, h) {
     const x2 = x + w;
     const y2 = y + h;
-    return (
-      "0\nLWPOLYLINE\n8\n0\n90\n4\n70\n1\n" +
-      "10\n" + num(x) + "\n20\n" + num(y) + "\n" +
-      "10\n" + num(x2) + "\n20\n" + num(y) + "\n" +
-      "10\n" + num(x2) + "\n20\n" + num(y2) + "\n" +
-      "10\n" + num(x) + "\n20\n" + num(y2) + "\n"
-    );
+    const pts = [
+      [x, y],
+      [x2, y],
+      [x2, y2],
+      [x, y2],
+    ];
+    let s =
+      pair(0, "POLYLINE") +
+      pair(8, "0") +
+      pair(66, 1) +
+      pair(70, 1);
+    for (let i = 0; i < pts.length; i++) {
+      s +=
+        pair(0, "VERTEX") +
+        pair(8, "0") +
+        pair(10, num(pts[i][0])) +
+        pair(20, num(pts[i][1]));
+    }
+    return s + pair(0, "SEQEND") + pair(8, "0");
   }
 
   function buildDxf(layout) {
+    const ext = bounds(layout);
     let entities = "";
     for (const r of layout.rects) {
       entities += rectPolyline(r.x, r.y, r.w, r.h);
     }
 
     return (
-      "0\nSECTION\n2\nHEADER\n9\n$INSUNITS\n70\n4\n" +
-      "0\nENDSEC\n0\nSECTION\n2\nENTITIES\n" +
+      section("HEADER") +
+      pair(9, "$ACADVER") +
+      pair(1, "AC1009") +
+      pair(9, "$INSUNITS") +
+      pair(70, 4) +
+      pair(9, "$MEASUREMENT") +
+      pair(70, 1) +
+      pair(9, "$EXTMIN") +
+      pair(10, num(ext.minX)) +
+      pair(20, num(ext.minY)) +
+      pair(30, "0.0") +
+      pair(9, "$EXTMAX") +
+      pair(10, num(ext.maxX)) +
+      pair(20, num(ext.maxY)) +
+      pair(30, "0.0") +
+      endSection() +
+      section("ENTITIES") +
       entities +
-      "0\nENDSEC\n0\nEOF\n"
+      endSection() +
+      pair(0, "EOF")
     );
   }
 
